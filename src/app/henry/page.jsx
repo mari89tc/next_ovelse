95818d7d956624002416e216753929d9e9535c00
Marie Kjærside <m.kjaerside@outlook.com>
2023-11-02 09:54:15 +0100

    hent api og vis det

---
+++ src/app/henry/page.jsx
@@ -0,0 +1,14 @@
+export default async function Henry() {
+  const res = await fetch("https://nice-dogs.vercel.app/api/dogs?slug=henry");
+  const data = await res.json();
+  const { name, favouriteColor, age, image } = data;
+  console.log(data);
+  return (
+    <main>
+      <h1>{name}</h1>
+      <p>{favouriteColor}</p>
+      <p>{age}</p>
+      <img src={image.url} alt="" />
+    </main>
+  );
+}
