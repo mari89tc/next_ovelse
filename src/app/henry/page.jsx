b8eedf6f2427b439aeb173040a6b9de5bf2e0769
Marie Kjærside <m.kjaerside@outlook.com>
2023-11-02 11:06:13 +0100

    image

--- src/app/henry/page.jsx
+++ src/app/henry/page.jsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 export async function generateMetadata() {
   const res = await fetch("https://nice-dogs.vercel.app/api/dogs?slug=henry");
   const data = await res.json();
@@ -9,14 +10,23 @@ export async function generateMetadata() {
 export default async function Henry() {
   const res = await fetch("https://nice-dogs.vercel.app/api/dogs?slug=henry");
   const data = await res.json();
-  const { name, favouriteColor, age, image } = data;
+  const { name, favouriteColor, age, image, width, height } = data;
   console.log(data);
   return (
     <main>
       <h1>{name}</h1>
       <p>{favouriteColor}</p>
       <p>{age}</p>
-      <img src={image.url} alt="" />
+      <Image
+        src={image.url}
+        alt="A cute dog"
+        width={image.width}
+        height={image.height}
+        priority={true}
+        sizes="(max-width: 768px) 100vw,
+         (max-width: 1200px) 50vw,
+         400px"
+      ></Image>
     </main>
   );
 }
